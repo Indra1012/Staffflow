12dc256f924c2ad7e1a4f86b898b4dbde030aed1
Indra1012 <mahindra10122002@gmail.com>
2026-04-09 00:17:42 +0530

    Auto-configured Render API linking and bulletproof CORS

--- server/server.js
+++ server/server.js
@@ -9,11 +9,7 @@ connectDB();
 const app = express();
 
 app.use(cors({
-  origin: [
-    process.env.CLIENT_URL || 'http://localhost:5173',
-    'http://localhost:5173',
-    'http://127.0.0.1:5173'
-  ],
+  origin: true, // Automatically reflects the request origin
   credentials: true,
   methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
   allowedHeaders: ['Content-Type', 'Authorization'],
